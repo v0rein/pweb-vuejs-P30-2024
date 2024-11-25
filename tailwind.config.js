b0ce8c2b3d0dc2fd608edad1f92ad43bb8d22cc6
AthallaITS <5027231018@student.its.ac.id>
2024-11-25 18:23:41 +0700

    fix: push error

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // Konfigurasi yang perlu ditambahkan:
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  // notes: sesuaikan file path dengan kebutuhan kalian dan pastikan file path-nya benar
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
